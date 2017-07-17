2845ed5618efc921c13d15444661120fe9190aa4
我也没有时光机器 <100885521@qq.com>
2017-07-17 20:04:06 +0800

    Initial

---
+++ MRButtonCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRButtonCollection
+//
+//  Created by MrXir on 2017/7/17.
+//  Copyright © 2017年 MrXir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-MRButtonCollection/Pods-MRButtonCollection-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MRButtonCollection : NSObject
+@end
+@implementation PodsDummy_Pods_MRButtonCollection
+@end
